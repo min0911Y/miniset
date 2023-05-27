634194f2e2c3b060e4f4d8d541c2ec75551193f3
min0911Y <86399932+min0911Y@users.noreply.github.com>
2023-05-27 15:23:40 +0800

    fix bugs and add a few comments

--- mst.c
+++ mst.c
@@ -18,11 +18,6 @@ void Free(void* ptr) {
 #define malloc Malloc
 #define free Free
 #endif
-/*
-  put_token:
-  参数：buf token字符串
-
-*/
 PRIVATE void put_token(char* buf, TOKEN_TYPE t, MST_Object* mst) {
   TOKEN* tok = malloc(sizeof(TOKEN));
   tok->t = t;
@@ -31,15 +26,21 @@ PRIVATE void put_token(char* buf, TOKEN_TYPE t, MST_Object* mst) {
 }
 PRIVATE char* __next(char* buf) {
   char *x, *y;
-  x = strchr(buf, ' ');
-  y = strchr(buf, '\t');
+  x = strchr(buf, ' ');   // 找到最近的空格
+  y = strchr(buf, '\t');  // 找到最近的缩进
+  /*
+    如果x、y都不为零，那就取小的那个
+    如果x、y都为0，那就返回0
+    若是x、y其中一个为0，那就返回不为0的那个
+  */
   return x < y ? (x != 0 ? x : y) : (y != 0 ? y : x);
 }
 PRIVATE char* _next(char* buf) {
-  if (buf[0] == '#') {
-    return buf;  // do nothing
+  if (buf[0] == '#') {  // 是注释，我们不用做任何事情
+    return buf;         // do nothing
   }
   char *x, *y;
+  /* 下一个token的分隔符是\n还是空格或缩进，因此执行一遍__next函数*/
   x = __next(buf);
   y = strchr(buf, '\n');
   return x < y ? (x != 0 ? x : y) : (y != 0 ? y : x);
@@ -48,22 +49,24 @@ PRIVATE char* next1(char* buf) {
   char* p = _next(buf);
   if (!(p))
     return 0;
-  while (p[0] == '#') {
-    p = strchr(p, '\n');
+  while (
+      p[0] ==
+      '#') {  // 跳过注释，下面的代码执行后，p指针就会指向下一行，因此，只要下一行不是注释，就可以退出循环了
+    p = strchr(p, '\n');  // 下一行
     if (p) {
-      p++;
-      if (!(*p)) {
+      p++;  // 此时*p是'\n'我们需要跳过一格，然后才是下一行的起始位置
+      if (!(*p)) {  // 遇到了字符串结束符，也相当于没有token了，返回0
         return 0;
       }
     } else {
       return 0;
     }
   }
-  while (*p == ' ' || *p == '\t' || *p == '\n') {
+  while (*p == ' ' || *p == '\t' || *p == '\n') {  // 找到有字符的地方
     p++;
   }
-  if (*p) {
-    while (p[0] == '#') {
+  if (*p) {                // 如果字符串没到结尾
+    while (p[0] == '#') {  // 继续跳过注释
       p = strchr(p, '\n');
       if (p) {
         p++;
@@ -74,7 +77,8 @@ PRIVATE char* next1(char* buf) {
         return 0;
       }
     }
-    while ((*p == ' ' || *p == '\t' || *p == '\n') && *p) {
+    while ((*p == ' ' || *p == '\t' || *p == '\n') &&
+           *p) {  // 继续跳过没字符的地方，于是我们就到了下一个有字符的地方
       p++;
     }
     return *p ? p : 0;
@@ -82,46 +86,55 @@ PRIVATE char* next1(char* buf) {
     return 0;
 }
 PRIVATE char* next(char* buf) {
-  char* p = next1(buf);
-  if (!p) {
+  char* p =
+      next1(buf);  // 其实上面注释什么的还没有处理完，所以现在我们进行下一步处理
+  // next1函数处理的是注释符号在行的开头的情况，我们现在要处理注释和正常字符混合在一起的情况
+  if (!p) {  // 上一步都没返回成功，直接0了
     return 0;
   }
   char* p1 = p;
-  int fg = 0;
+  int fg = 0;  // 字符串标识符，这里是为了防止字符串里的注释也被误判
   while ((*p != '#' && fg) && *p != ' ' && *p) {
     if (*p == '\"') {
       fg = !fg;
     }
     p++;
   }
-  if (*p != '#') {
+  // 此时p代表的就是下一个注释的起始位置
+  if (*p != '#') {  // 没有注释？直接返回了
     return p1;
   } else {
-    while (*p != '\n' && *p != '\0') {
+    while (*p != '\n' && *p != '\0') {  // 把这个注释删了
       *p = ' ';
       p++;
     }
     return p1;
   }
 }
-PRIVATE char* get_str(char* text, char** p) {
+PRIVATE char* get_str(char* text,
+                      char** p) {  // 从next返回的token中获取有效字符串
   size_t len = 0;
   char* src = text;
   char* result;
-  int flag_str = 0;
-  int flag_str1 = 0;
-  int flag_op = 0;
+  int flag_str = 0;   // 字符串标识
+  int flag_str1 = 0;  // 字符串内转义字符标识
+  int flag_op = 0;    // 数组标识
   while (*text) {
-    // printf("%c\n",*text);
     len++;
-    if (!flag_str && *text == '[') {
-      flag_op = 1;
+    if (!flag_str &&
+        *text == '[') {  // 不在字符串内并且是数组的开始，设置flag_op标识(+1 代表嵌套的层数)
+      flag_op++;
     }
-    if (!flag_str && *text == ']') {
-      flag_op = 0;
+    if (!flag_str &&
+        *text == ']') {  // 不在字符串内并且是数组的结束，结束flag_op标识（-1 少一个嵌套的层数）
+      if (!flag_op) {  // 只有闭合，没有开始，报错
+        return NULL;
+      }
+      flag_op--;
     }
+    /* 处理字符串 */
     if (!flag_op) {
-      if (*text == '\\') {
+      if (*text == '\\') { // 转义字符
         flag_str1 = !flag_str1;
       } else if (flag_str1) {
         flag_str1 = 0;
@@ -447,11 +460,26 @@ PRIVATE int parser_space(MST_Object* mst, int idx, SPACE* space, int mode) {
     if (t->t == WORD) {
       Var* v = (Var*)malloc(sizeof(Var));
       v->name = t->tok;
-      //      printf("v->name = %ll16x\n",v->name);
-      TOKEN* t1 = (TOKEN*)FindForCount(i + 2, mst->token)->val;
+      List* tk_list1 = FindForCount(i + 1, mst->token);
+      if (!tk_list1) {
+        mst->err = SYNTAX_ERROR;
+        return -1;
+      }
+      TOKEN* t2 = (TOKEN*)tk_list1->val;
+      if (t2->t != OP) {
+        free(v);
+        mst->err = SYNTAX_ERROR;
+        return -1;
+      }
+      List* tk_list2 = FindForCount(i + 2, mst->token);
+      if (!tk_list2) {
+        mst->err = SYNTAX_ERROR;
+        return -1;
+      }
+      TOKEN* t1 = (TOKEN*)tk_list2->val;
       if (t1->t == SPACE_START) {
-        //printf("START\n");
-        //   printf("Found a Space %s\n",t->tok);
+        // printf("START\n");
+        //    printf("Found a Space %s\n",t->tok);
         v->vt = SPAC;
         v->obj = (SPACE*)malloc(sizeof(SPACE));
         SPACE* sp = (SPACE*)v->obj;
@@ -465,7 +493,7 @@ PRIVATE int parser_space(MST_Object* mst, int idx, SPACE* space, int mode) {
           return -1;
         }
         AddVal((uintptr_t)v, space->the_space);
-        //i += 2;
+        // i += 2;
       } else if (t1->t == ARRAY_START) {
         //    printf("Found a Array:%s\n",t->tok);
         v->vt = ARRAY;
@@ -498,10 +526,13 @@ PRIVATE int parser_space(MST_Object* mst, int idx, SPACE* space, int mode) {
         String* str = (String*)v->obj;
 
         str->str = t1->tok;
-      //  printf("string = %llx %llx\n", str->str, t1->tok);
+        //  printf("string = %llx %llx\n", str->str, t1->tok);
         AddVal((uintptr_t)v, space->the_space);
         i += 2;
       }
+    } else {
+      mst->err = SYNTAX_ERROR;
+      return -1;
     }
   }
   if (!flag && space != MST_GetRootSpace(mst)) {
