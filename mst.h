98d6bf5eee96538b69bb93576c72bf3241c338e4
min0911Y <1474635462@qq.com>
2024-04-25 21:31:59 +0800

    upd

--- mst.h
+++ mst.h
@@ -1,11 +1,17 @@
-#ifndef _MST_H
-#define _MST_H
+#ifndef MST_H
+#define MST_H
+#ifdef __cplusplus
+extern "C" {
+#endif
 #define PRIVATE static
 #define PUBLIC
 #define MST_API
 #include <stddef.h>
 #include <stdint.h>
-//#define MEM_LEAK_CHK
+#define MEM_LEAK_CHECK 0
+#if MEM_LEAK_CHECK
+#include "leakcheck.h"
+#endif
 struct ListCtl {
   struct List* start;
   struct List* end;
@@ -19,14 +25,14 @@ struct List {
 };
 typedef struct List List;
 typedef enum {
-  NUMBER,
-  STRING,
-  SPACE_START,
-  SPACE_END,
-  ARRAY_START,
-  ARRAY_END,
-  OP,
-  WORD
+  TK_NUMBER, // 数字标识符
+  TK_STRING, // 字符串标识符
+  TK_SPACE_START, // SPACE变量的开始
+  TK_SPACE_END, // SPACE变量的结束
+  TK_ARRAY_START, // 数组的开始
+  TK_ARRAY_END, // 数组的结束
+  TK_OP, // 符号标识符
+  TK_WORD // 变量的名字
 } TOKEN_TYPE;
 typedef struct {
   char* tok;
@@ -39,7 +45,7 @@ typedef enum {
   SYNTAX_ERROR,
   WRONG_TYPE_TO_ADD
 } ERR_TYPE;
-typedef enum { INTEGER, ARRAY, SPAC, STR } VAR_TYPE;
+typedef enum { VAR_INTEGER, VAR_ARRAY, VAR_SPACE, VAR_STRING } VAR_TYPE;
 typedef struct {
   List* the_space;
 } SPACE;
@@ -63,10 +69,12 @@ typedef struct {
   VAR_TYPE vt;
   char* name;
   void* obj;
+  List* this_list;
 } Var;
 typedef struct {
   VAR_TYPE vt;
   void* obj;
+  List* this_list;
 } Array_data;
 /*
     mini set 解析路线
@@ -76,25 +84,32 @@ typedef struct {
     Space中放Var，Var中可以放Space、Array、integer、string
     Parser会设置一个叫做`root_space`的空间以存放Var
  */
-void AddVal(uintptr_t val, struct List* Obj);
-struct List* FindForCount(size_t count, struct List* Obj);
-void DeleteVal(size_t count, struct List* Obj);
-struct List* NewList(void);
-void Change(size_t count, struct List* Obj, uintptr_t val);
-int GetLastCount(struct List* Obj);
-void DeleteList(struct List* Obj);
-PUBLIC MST_API MST_Object* Init_MstObj(char* string);
-PUBLIC MST_API Var* MST_GetVar(char* name, SPACE* space);
-PUBLIC MST_API int MST_Space_GetInteger(Var* var);
-PUBLIC MST_API SPACE* MST_Space_GetSpace(Var* var);
-PUBLIC MST_API Array* MST_Space_GetArray(Var* var);
-PUBLIC MST_API char* MST_Space_GetStr(Var* var);
-PUBLIC MST_API Array_data* MST_Array_Get(Array* arr, int idx);
-PUBLIC MST_API int MST_Array_get_integer(Array_data* ad);
-PUBLIC MST_API char* MST_Array_get_str(Array_data* ad);
-PUBLIC MST_API SPACE* MST_Array_get_space(Array_data* ad);
-PUBLIC MST_API Array* MST_Array_get_array(Array_data* ad);
-PUBLIC MST_API void MST_FreeObj(MST_Object* mst);
+
+// 链表
+List* list_add_val(uintptr_t val, struct List* Obj);
+struct List* list_search_by_count(size_t count, struct List* Obj);
+void list_delete_by_count(size_t count, struct List* Obj);
+struct List* list_new(void);
+void list_change_child_by_count(size_t count, struct List* Obj, uintptr_t val);
+size_t list_get_last_count(struct List* Obj);
+void list_delete(struct List* Obj);
+void list_delete_child(struct List* need_to_free, struct List* Obj);
+
+PUBLIC void free_space(SPACE* space);
+PUBLIC void free_arr(Array* arr);
+// API
+PUBLIC MST_API MST_Object* MST_init(char* string);
+PUBLIC MST_API Var* MST_get_var(char* name, SPACE* space);
+PUBLIC MST_API int MST_space_get_integer(Var* var);
+PUBLIC MST_API SPACE* MST_space_get_space(Var* var);
+PUBLIC MST_API Array* MST_space_get_array(Var* var);
+PUBLIC MST_API char* MST_space_get_str(Var* var);
+PUBLIC MST_API Array_data* MST_array_get_data(Array* arr, int idx);
+PUBLIC MST_API int MST_array_get_integer(Array_data* ad);
+PUBLIC MST_API char* MST_array_get_str(Array_data* ad);
+PUBLIC MST_API SPACE* MST_array_get_space(Array_data* ad);
+PUBLIC MST_API Array* MST_array_get_array(Array_data* ad);
+PUBLIC MST_API void MST_free(MST_Object* mst);
 
 PUBLIC MST_API const char* MST_strerror(MST_Object* mst);
 PUBLIC MST_API void MST_add_data_to_array(MST_Object* mst_obj,
@@ -108,10 +123,8 @@ PUBLIC MST_API Var MST_var_make_integer(char* name, int val);
 PUBLIC MST_API Var MST_var_make_string(char* name, char* ss);
 PUBLIC MST_API Array_data MST_arr_dat_make_integer(int val);
 PUBLIC MST_API Array_data MST_arr_dat_make_string(char* ss);
-PUBLIC MST_API void MST_add_empty_array_to_array(MST_Object* mst_obj,
-                                                 Array* arr);
-PUBLIC MST_API void MST_add_empty_space_to_array(MST_Object* mst_obj,
-                                                 Array* arr);
+PUBLIC MST_API void MST_add_empty_array_to_array(Array* arr);
+PUBLIC MST_API void MST_add_empty_space_to_array(Array* arr);
 PUBLIC MST_API void MST_add_empty_array_to_space(MST_Object* mst_obj,
                                                  SPACE* sp,
                                                  char* name);
@@ -122,34 +135,36 @@ PUBLIC MST_API void MST_change_var(MST_Object* mst_obj, Var v, Var* v1);
 PUBLIC MST_API void MST_change_arr(MST_Object* mst_obj,
                                    Array_data v,
                                    Array_data* v1);
-#define MST_GetRootSpace(m) (m)->root_space
+PUBLIC MST_API void MST_remove_var(Var* v);
+PUBLIC MST_API void MST_remove_arr(Array_data* v);
+#define MST_get_root_space(m) (m)->root_space
 // MST_get_space_in_space
 #define MST_get_space_in_space(m, name, space)          \
-  MST_GetVar((name), (space))                           \
-      ? MST_Space_GetSpace(MST_GetVar((name), (space))) \
+  MST_get_var((name), (space))                           \
+      ? MST_space_get_space(MST_get_var((name), (space))) \
       : NULL
 #define MST_get_space_in_array(m, idx, arr)              \
-  MST_Array_Get((arr), (idx))                            \
-      ? MST_Array_get_space(MST_Array_Get((arr), (idx))) \
+  MST_array_get_data((arr), (idx))                            \
+      ? MST_array_get_space(MST_array_get_data((arr), (idx))) \
       : NULL  // MST_get_space_in_array
 #define MST_get_integer_in_space(m, name, space)          \
-  MST_GetVar((name), (space))                             \
-      ? MST_Space_GetInteger(MST_GetVar((name), (space))) \
+  MST_get_var((name), (space))                             \
+      ? MST_space_get_integer(MST_get_var((name), (space))) \
       : -1  // MST_get_integer_in_space
 #define MST_get_integer_in_array(m, idx, arr)              \
-  MST_Array_Get((arr), (idx))                              \
-      ? MST_Array_get_integer(MST_Array_Get((arr), (idx))) \
+  MST_array_get_data((arr), (idx))                              \
+      ? MST_array_get_integer(MST_array_get_data((arr), (idx))) \
       : -1  // MST_get_integer_in_array
 #define MST_get_string_in_space(m, name, space)                               \
-  MST_GetVar((name), (space)) ? MST_Space_GetStr(MST_GetVar((name), (space))) \
+  MST_get_var((name), (space)) ? MST_space_get_str(MST_get_var((name), (space))) \
                               : NULL  // MST_get_string_in_space
 #define MST_get_string_in_array(m, idx, arr)                                   \
-  MST_Array_Get((arr), (idx)) ? MST_Array_get_str(MST_Array_Get((arr), (idx))) \
+  MST_array_get_data((arr), (idx)) ? MST_array_get_str(MST_array_get_data((arr), (idx))) \
                               : NULL  // MST_get_string_in_array
 #define MST_change_var_for_name(m, v, name, space)           \
   {                                                          \
-    if (MST_GetVar((name), (space))) {                       \
-      MST_change_var((m), (v), MST_GetVar((name), (space))); \
+    if (MST_get_var((name), (space))) {                       \
+      MST_change_var((m), (v), MST_get_var((name), (space))); \
     }                                                        \
   }
 #define MST_change_arr_for_idx(m, v, idx, arr)               \
@@ -158,4 +173,7 @@ PUBLIC MST_API void MST_change_arr(MST_Object* mst_obj,
       MST_change_arr((m), (v), MST_Array_Get((arr), (idx))); \
     }                                                        \
   }
+#ifdef __cplusplus
+}
+#endif
 #endif
